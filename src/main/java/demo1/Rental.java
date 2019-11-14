55686c1c43bbbdafc7856e2e45d7b37c9d300779
SaraQian <abigail830@163.com>
2019-11-14 11:46:01 +0800

    simplify calculateFrequentRenterPoints

--- src/main/java/demo1/Rental.java
+++ src/main/java/demo1/Rental.java
@@ -27,13 +27,10 @@ public class Rental {
     }
 
     public int calculateFrequentRenterPoints() {
-        // add frequent renter points （累计常客积点。
-        int frequentRenterPoints = 1;
-
-        // add bonus for a two day new release rental
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
+            return 2;
+        else {
+            return 1;
+        }
     }
 }
