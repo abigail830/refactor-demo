a5867ad70efad6cb97811e1a24bd4b645c2553bd
SaraQian <abigail830@163.com>
2019-11-14 11:35:09 +0800

    move method to Customer

--- src/main/java/demo1/StatementService.java
+++ src/main/java/demo1/StatementService.java
@@ -17,28 +17,7 @@ package demo1;
 public class StatementService {
 
     public String printStatement(Customer customer) {
-
-        double totalAmount = 0; // 总消费金。
-        int frequentRenterPoints = 0; // 常客积点
-
-        String result = "Rental Record for " + customer.getName() + "\n";
-
-        for (Rental rental : customer.getRentalList()) {
-
-            frequentRenterPoints = rental.calculateFrequentRenterPoints();
-
-            double thisAmount = rental.calculateAmount();
-            result += "\t" + rental.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
-
-        // add footer lines（结尾打印）
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
-                + " frequent renter points";
-
-        return result;
+        return customer.printStatement();
     }
 
 }
