1d72ed5f13f011100871ebae91b8b7ac07e2a60d
SaraQian <abigail830@163.com>
2019-11-20 22:50:43 +0800

    move printStatement to Customer

--- src/main/java/demo1/Customer.java
+++ src/main/java/demo1/Customer.java
@@ -32,4 +32,34 @@ public class Customer {
                 ", rentalList=" + rentalList +
                 '}';
     }
+
+    public String printStatement() {
+
+        double totalAmount = 0; // 总消费金。
+        int frequentRenterPoints = 0; // 常客积点
+
+        String result = "Rental Record for " + name + "\n";
+
+        for (Rental rental : rentalList) {
+
+            // add frequent renter points （累计常客积点。
+            frequentRenterPoints += rental.calculateFrequentRenterPoints();
+
+            // 取得影片出租价格
+            double thisAmount = rental.calculateAmount();
+
+            // show figures for this rental（显示此笔租借记录）
+            result += "\t" + rental.getMovie().getTitle() + "\t"
+                    + String.valueOf(thisAmount) + "\n";
+
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines（结尾打印）
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                + " frequent renter points";
+
+        return result;
+    }
 }
